31364440dc928bd342a3137d7b671dad174cf20c
alexisraimbault <42438383+alexisraimbault@users.noreply.github.com>
2023-04-09 19:36:12 +0200

    cors test

--- src/pages/auth/notioncallback/index.js
+++ src/pages/auth/notioncallback/index.js
@@ -9,33 +9,44 @@ const Home = () => {
   const supabase = useSupabaseClient()
   const user = useUser()
   const router = useRouter()
-  const notionCode = router.query.code
+  const { code } = router.query;
 
-  const notionIntegrationClientId = "e1ec6c09-bbde-449f-a5e1-d9e8a4aa582d"
-  const notionIntegrationClientSecret = "secret_mILEebhBUG7gUshPxnNRIPZN2ZAiO5vet9yPDdxCmGL"
-
-  const getNotionAccessToken = async () => {
-    const res = await fetch('https://api.notion.com/v1/oauth/token', {
-      method: 'post',
-      mode: 'no-cors',
-      headers: {
-        Authorization: `Basic ${notionIntegrationClientId}:${notionIntegrationClientSecret}`,
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-        grant_type: "authorization_code",
-        code: notionCode, 
-        redirect_uri: "http://localhost:3000/auth/notioncallback"
-      })
-    })
-
-    console.log({res})
-    const response = await res.json()
-    console.log({response})
-  }
+  useEffect(() => {
+    if(code && code !== null && code !== undefined && code.length > 0) {
+      getNotionAccessToken(code)
+    }
+  }, [code])
   
-  console.log({user})
+  const getNotionAccessToken = async (notionCode) => {
+    console.log({notionCode})
+    console.log('V1')
+    const notionIntegrationClientId = "e1ec6c09-bbde-449f-a5e1-d9e8a4aa582d"
+    const notionIntegrationClientSecret = "secret_mILEebhBUG7gUshPxnNRIPZN2ZAiO5vet9yPDdxCmGL"
+
+    try {
+      const res = await fetch('https://api.notion.com/v1/oauth/token', {
+        method: 'post',
+        // mode: 'no-cors',
+        headers: new Headers({
+          'Authorization': `Basic ${btoa(`${notionIntegrationClientId}:${notionIntegrationClientSecret}`)}`,
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        }),
+        body: JSON.stringify({
+          grant_type: "authorization_code",
+          code: notionCode, 
+          redirect_uri: "http://localhost:3000/auth/notioncallback"
+        })
+      })
+  
+      console.log({res})
+      // const response = await res.json()
+      // console.log({response})
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
   getNotionAccessToken()
 
   return (
