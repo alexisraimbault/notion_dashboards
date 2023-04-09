577cca59de34c85c86607fe2b0f9e676c7408d4a
alexisraimbault <42438383+alexisraimbault@users.noreply.github.com>
2023-04-09 18:38:27 +0200

    first commit

---
+++ src/pages/auth/notioncallback/index.js
@@ -0,0 +1,48 @@
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useRouter } from 'next/router'
+import { useSession, useSupabaseClient, useUser } from '@supabase/auth-helpers-react'
+import React, {useState, useEffect} from 'react'
+
+const Home = () => {
+  const session = useSession()
+  const supabase = useSupabaseClient()
+  const user = useUser()
+  const router = useRouter()
+  const notionCode = router.query.code
+
+  const notionIntegrationClientId = "e1ec6c09-bbde-449f-a5e1-d9e8a4aa582d"
+  const notionIntegrationClientSecret = "secret_mILEebhBUG7gUshPxnNRIPZN2ZAiO5vet9yPDdxCmGL"
+
+  const getNotionAccessToken = async () => {
+    const res = await fetch('https://api.notion.com/v1/oauth/token', {
+      method: 'post',
+      mode: 'no-cors',
+      headers: {
+        Authorization: `Basic ${notionIntegrationClientId}:${notionIntegrationClientSecret}`,
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        grant_type: "authorization_code",
+        code: notionCode, 
+        redirect_uri: "http://localhost:3000/auth/notioncallback"
+      })
+    })
+
+    console.log({res})
+    const response = await res.json()
+    console.log({response})
+  }
+  
+  console.log({user})
+  getNotionAccessToken()
+
+  return (
+    <div className="container" style={{ padding: '50px 0 100px 0' }}>
+      
+    </div>
+  )
+}
+
+export default Home
