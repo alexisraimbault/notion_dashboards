791928f50d19481bf239153521a448adea20c6fd
alexisraimbault <27raimbault.alexis@gmail.com>
2023-08-17 14:46:21 +0200

    added color palettes

--- src/components/GraphRender.js
+++ src/components/GraphRender.js
@@ -4,6 +4,30 @@ import React, {useState, useEffect} from 'react'
 import { LineChart, Line, Tooltip, XAxis, YAxis, ResponsiveContainer, BarChart, Bar, PieChart, Pie, Cell, CartesianGrid, Legend } from 'recharts';
 
 const GraphRenderer = ({graphData, graphSettings}) => {
+    // PALETTES
+
+    // Divergent
+    const palette1 = ["#00876c", "#4b9d72", "#79b37a", "#a5c884", "#d2dd92", "#fff1a5", "#fad083", "#f4af69", "#ed8b58", "#e36651", "#d43d51"]
+
+    // Blues
+    const palette2 = ['#004c6d','#005e81','#007094','#0083a7','#0096ba','#00aacc','#00bede','#00d3ef','#00e8ff']
+
+    // Palette
+    const palette3 = ['#003f5c','#2f4b7c','#665191','#a05195','#d45087','#f95d6a','#ff7c43','#ffa600']
+
+    // pastels
+    const palette4 = ["#fd7f6f", "#7eb0d5", "#b2e061", "#bd7ebe", "#ffb55a", "#ffee65", "#beb9db", "#fdcce5", "#8bd3c7"]
+
+    // colored retro
+    const palette5 = ["#ea5545", "#f46a9b", "#ef9b20", "#edbf33", "#ede15b", "#bdcf32", "#87bc45", "#27aeef", "#b33dc6"]
+
+    // colored bright
+    const palette6 = ["#e60049", "#0bb4ff", "#50e991", "#e6d800", "#9b19f5", "#ffa300", "#dc0ab4", "#b3d4ff", "#00bfa0"]
+
+    // blue to yellow
+    const palette7 = ["#115f9a", "#1984c5", "#22a7f0", "#48b5c4", "#76c68f", "#a6d75b", "#c9e52f", "#d0ee11", "#d0f400"]
+
+    const palettes = [palette1, palette2, palette3, palette4, palette5, palette6, palette7]
 
     const isNumeric = toTest => {
         return !isNaN(toTest)
@@ -71,6 +95,15 @@ const GraphRenderer = ({graphData, graphSettings}) => {
     const isYPropertyNumeric = YaxisData?.isNumeric
     const aggregationType = !isYPropertyNumeric ? 'count' : graphSettings?.aggregation || 'sum'
     const chartTypeRaw = graphSettings?.type || 'line'
+
+    let paletteIndex = parseInt(graphSettings?.palette || 3)
+    const availablePalettes = [...Array(palettes.length).keys()]
+
+    if(!availablePalettes.includes(paletteIndex - 1)) {
+        paletteIndex = 3
+    }
+
+    const paletteToUse = palettes[paletteIndex - 1]
 
     const availableTypes = ['line', 'bar', 'pie']
     const chartType = availableTypes.includes(chartTypeRaw) ? chartTypeRaw : 'line'
@@ -161,48 +194,6 @@ const GraphRenderer = ({graphData, graphSettings}) => {
 
     const formattedData = formatGraphData()
 
-    // RENDERS
-
-    // Divergent
-    const palette1 = [
-        '#00876c',
-        '#4b9c70',
-        '#78b076',
-        '#a3c37f',
-        '#cfd68d',
-        '#fae89f',
-        '#f5ca7f',
-        '#f1aa67',
-        '#eb8857',
-        '#e16551',
-        '#d43d51',
-    ]
-
-    // Blues
-    const palette2 = [
-        '#004c6d',
-        '#005e81',
-        '#007094',
-        '#0083a7',
-        '#0096ba',
-        '#00aacc',
-        '#00bede',
-        '#00d3ef',
-        '#00e8ff',
-    ]
-
-    // Palette
-    const palette3 = [
-        '#003f5c',
-        '#2f4b7c',
-        '#665191',
-        '#a05195',
-        '#d45087',
-        '#f95d6a',
-        '#ff7c43',
-        '#ffa600',
-    ]
-
     const CustomTooltip = ({ active, payload, label }) => {
         return (
             <div className='graph-view__tooltip-wrapper'>
@@ -219,7 +210,7 @@ const GraphRenderer = ({graphData, graphSettings}) => {
         <ResponsiveContainer width="100%" height={"100%"}>
           <LineChart data={formattedData}>
             <CartesianGrid strokeDasharray="3 3" />
-            <Line type="monotone" dataKey="YItemData" stroke={palette3[0]} />
+            <Line type="monotone" dataKey="YItemData" stroke={paletteToUse[0]} />
             <XAxis dataKey="XItemData" />
             <YAxis />
             {/* <YAxis dataKey="YItemData" /> */}
@@ -238,7 +229,7 @@ const GraphRenderer = ({graphData, graphSettings}) => {
                 <YAxis />
                 <Bar dataKey="YItemData" fill="var(--custom-red)" >
                 {
-                    formattedData.map((entry, index) => <Cell key={`color-bar-${index}`} fill={palette3[index % palette3.length]}/>)
+                    formattedData.map((entry, index) => <Cell key={`color-bar-${index}`} fill={paletteToUse[index % paletteToUse.length]}/>)
                 }
                 </Bar>
                 <Tooltip content={<CustomTooltip />} />
@@ -261,7 +252,7 @@ const GraphRenderer = ({graphData, graphSettings}) => {
                 outerRadius={"80%"} 
             >
                 {
-                formattedData.map((entry, index) => <Cell key={`color-cell-${index}`} fill={palette3[index % palette3.length]}/>)
+                formattedData.map((entry, index) => <Cell key={`color-cell-${index}`} fill={paletteToUse[index % paletteToUse.length]}/>)
                 }
             </Pie>
             <Tooltip />
---
+++ src/pages/api/graph/palettes.js
@@ -0,0 +1,77 @@
+import { NextApiHandler } from 'next'
+import { createClient } from '@supabase/supabase-js'
+
+
+const handler = async (req, res) => {
+    // Divergent
+    const palette1 = ["#00876c", "#4b9d72", "#79b37a", "#a5c884", "#d2dd92", "#fff1a5", "#fad083", "#f4af69", "#ed8b58", "#e36651", "#d43d51"]
+
+    // Blues
+    const palette2 = ['#004c6d','#005e81','#007094','#0083a7','#0096ba','#00aacc','#00bede','#00d3ef','#00e8ff']
+
+    // Palette
+    const palette3 = ['#003f5c','#2f4b7c','#665191','#a05195','#d45087','#f95d6a','#ff7c43','#ffa600']
+
+    // pastels
+    const palette4 = ["#fd7f6f", "#7eb0d5", "#b2e061", "#bd7ebe", "#ffb55a", "#ffee65", "#beb9db", "#fdcce5", "#8bd3c7"]
+
+    // colored retro
+    const palette5 = ["#ea5545", "#f46a9b", "#ef9b20", "#edbf33", "#ede15b", "#bdcf32", "#87bc45", "#27aeef", "#b33dc6"]
+
+    // colored bright
+    const palette6 = ["#e60049", "#0bb4ff", "#50e991", "#e6d800", "#9b19f5", "#ffa300", "#dc0ab4", "#b3d4ff", "#00bfa0"]
+
+    // blue to yellow
+    const palette7 = ["#115f9a", "#1984c5", "#22a7f0", "#48b5c4", "#76c68f", "#a6d75b", "#c9e52f", "#d0ee11", "#d0f400"]
+
+    const palettes = [
+        {
+            number: 1,
+            name: 'Divergent',
+            colors: palette1,
+            url: 'https://wizetables.com/divergent.png'
+        },
+        {
+            number: 2,
+            name: 'Blues',
+            colors: palette2,
+            url: 'https://wizetables.com/blues.png'
+        },
+        {
+            number: 3,
+            name: 'Purple Classic',
+            colors: palette3,
+            url: 'https://wizetables.com/classic.png'
+        },
+        {
+            number: 4,
+            name: 'Pastels',
+            colors: palette4,
+            url: 'https://wizetables.com/pastels.png'
+        },
+        {
+            number: 5,
+            name: 'Retro',
+            colors: palette5,
+            url: 'https://wizetables.com/retro.png'
+        },
+        {
+            number: 6,
+            name: 'Bright',
+            colors: palette6,
+            url: 'https://wizetables.com/bright.png'
+        },
+        {
+            number: 7,
+            name: 'Blue To Yellow',
+            colors: palette7,
+            url: 'https://wizetables.com/blue_to_yellow.png'
+        },
+    ]
+
+    res.status(200).json({ 
+        palettes,
+    })
+}
+
+export default handler
